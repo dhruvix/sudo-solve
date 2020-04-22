d26565a67ad6a6d7cf3c4c4828c8f04c48a8d369
dhruvix <dhruvix7@gmail.com>
2020-04-22 22:40:05 +0530

    Updates

---
+++ precache-manifest.675b68c0136b44aeabfb6a55f328451c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a70e9543c37c4e3165f0c1277edc7f",
+    "url": "/sudo-solve/index.html"
+  },
+  {
+    "revision": "34a0b32ed1e935a50f09",
+    "url": "/sudo-solve/static/css/main.fcc772a4.chunk.css"
+  },
+  {
+    "revision": "e9a4c5eb21affcb36665",
+    "url": "/sudo-solve/static/js/2.3ba0a05e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sudo-solve/static/js/2.3ba0a05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a0b32ed1e935a50f09",
+    "url": "/sudo-solve/static/js/main.9299f9aa.chunk.js"
+  },
+  {
+    "revision": "ba8126273cad6aee8121",
+    "url": "/sudo-solve/static/js/runtime-main.eed512b5.js"
+  },
+  {
+    "revision": "7622201453ade1c61ac509297062b9a7",
+    "url": "/sudo-solve/static/media/DV.76222014.jpg"
+  }
+]);
